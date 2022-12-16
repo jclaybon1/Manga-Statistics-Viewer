9dce705b1f307cb54b2d5d6bcaf813be027d54da
Josh <you@example.com>
2022-12-16 02:00:13 -0600

    Update mangaStats.js

--- my-app/src/components/mangaStats.js
+++ my-app/src/components/mangaStats.js
@@ -5,27 +5,15 @@ import { Form, Card } from 'react-bootstrap';
 
 var url = "https://api.mangadex.org/manga";
 const imgURL = "https://mangadex.org/covers/";
-// const title = "?title=";
-// const limit = "&limit=";
-// const includeCoverArt = "&includes[]=cover_art";
-// const limitNum = 12;
 
-
-// import React, { useState } from 'react';
 
 export function Example() {
-  // Declare a new state variable, which we'll call "count"
   const [search, setSearch] = useState(" ");
   const [mangas, setMangas] = useState([]);
-//   const title = "?title=";
-    // const limit = "&limit=";
-    // const includeCoverArt = "&includes[]=cover_art";
-    // const limitNum = 12;
+
   const baseUrl = 'https://api.mangadex.org';
-//   const state ={value: ''};
   const state = useRef();
 
-//   let requestHttp = url + title + search.trim() + limit + String(limitNum) + includeCoverArt;
 
   useEffect( () => {
     // alert(search);
@@ -46,9 +34,7 @@ export function Example() {
             let fileName = mangaResult.relationships.find((relationship) => {
                 return relationship.type === "cover_art";
             })?.attributes?.fileName;
-            // let coverArt = mangaResult.relationships.find((relationship) => {
-            //     return relationship.type === "cover_art";
-            // });
+            
             // console.log(fileName);
             let coverURL = imgURL + mangaResult.id + "/" + fileName + ".256.jpg";
             // console.log(coverURL);
@@ -81,13 +67,6 @@ export function Example() {
         console.log(mangas);
         
 
-
-        // return resp1;
-        // const data = fetchData().toPromise();
-
-        // console.log(data);
-        
-
     }
     fetchData();
   
@@ -118,12 +97,7 @@ export function Example() {
                     {
                         mangas.length ?
                             mangas.map(manga => {
-                                // return (
-                                //     // <Card className='list-card' key={manga.id}>
-                                //     //     <Card.Img className="card-img"  src={manga.url}></Card.Img>
-                                //     //     <Card.Title className='card-title'>{Object.values(manga.manga.attributes.title)[0] ? Object.values(manga.manga.attributes.title)[0] : "Error"}</Card.Title>
-                                //     // </Card> 
-                                // );
+                                
                                 return(
                                     <div>
                                         <img src={manga.url}></img>
@@ -144,11 +118,6 @@ export function Example() {
         
      
     </div>
-
-
-    // <div className='c-manga-search'>
-    //     <Form.Control type="text" onChange={event => { setSearch(event.target.value);  }} ></Form.Control>
-    // </div >
 
   );
 }
